ebdc4b79d36c3595840e109198b2421e66b798fa
HamidOmarr <89842207+HamidOmarr@users.noreply.github.com>
2023-02-15 13:15:49 +0100

    Add files via upload

--- Yathzee.py
+++ Yathzee.py
@@ -1,39 +1,55 @@
 import random
 
-TotaalOpslag = []
-totaal = []
-punten = 0
-ronde = 1
-pogingen = 3
-dobbelsteen = [1,2,3,4,5,6]
+def rolldobbelsteen():
+    return [random.randint(1, 6) for _ in range(5)]
 
+def rerolldobbelsteen(dice):
+    keep = input("Welke dobbelsteen wil je houden? (comma gescheiden) ")
+    keep = [int(d) for d in keep.split(",") if d.isdigit()]
+    return keep + [random.randint(1, 6) for _ in range(5 - len(keep))]
 
-while pogingen > 0: 
-    print ("ronde:", ronde)
-    print ("poging:", pogingen)
-    print ("Punten:", punten)
-    for x in range(1,6):
-        totaal.append(random.choice(dobbelsteen))
-    print (totaal)
-    Nogeenkeer = input("Wil je nog een keer gooien? (3 kansen)")
-    if Nogeenkeer == "J":
-        for x in range (len(totaal)):
-            OpslagRol = input(print("zou jij", totaal[x] ,"willen houden?"))
-            if OpslagRol == "J":
-                TotaalOpslag.append(totaal[x])
-        print (str(TotaalOpslag))
-        if '3' in TotaalOpslag:
-            punten += 1
-            
-        pogingen = pogingen - 1
-        totaal.clear()
-  
-    if Nogeenkeer == "N" or pogingen <= 0: 
-        pogingen = 3 
-        ronde = ronde + 1
-        TotaalOpslag.clear()
-        totaal.clear()
+def scoredobbelsteen(dice):
+    score = {}
+    for d in dice:
+        if d in score:
+            score[d] += d
+        else:
+            score[d] = d
+    if len(score) == 5:
+        return 50  # Yahtzee!
+    if len(score) == 4:
+        return sum(dice)
+    if len(score) == 3:
+        if any(v == 3 for v in score.values()):
+            return 25  # Full House
+        else:
+            return sum(dice)
+    if len(score) == 2:
+        if any(v == 4 for v in score.values()):
+            return sum(dice)  # Four of a Kind
+        else:
+            return 30  # Small Straight
+    if len(score) == 1:
+        return sum(dice)  # Chance
+    return 0
 
+def play_yahtzee():
+    scores = {}
+    for i in range(13):
+        print(f"\nRound {i+1}")
+        dice = rolldobbelsteen()
+        print(f"Roll 1: {dice}")
+        dice = rerolldobbelsteen(dice)
+        print(f"Roll 2: {dice}")
+        dice = rerolldobbelsteen(dice)
+        print(f"Roll 3: {dice}")
+        score = scoredobbelsteen(dice)
+        print(f"Score: {score}")
+        categorie = input("welke categorie wil je in scoren? ")
+        scores[categorie] = score
+    print("\nFinal scores:")
+    for categorie, score in scores.items():
+        print(f"{categorie}: {score}")
 
-        
-        
+play_yahtzee()    
+    
